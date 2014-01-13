3dfa781ce8e073f40eda3d80794ad1caff5d5920
Owen Evans <OEvans@esri.com>
2014-01-13 08:18:49 -0500

    Enhance comments in Migrate Account sample

--- samples/migrateAccount.py
+++ samples/migrateAccount.py
@@ -1,15 +1,17 @@
-#### Migrate person to a new account within the same Org
+#### Migrate a member to a new account within the same Organization
 
 # Requires admin role
-# Useful when migrating to Enterprise Logins.
-# Reassigns all items/groups to new owner and
-# adds userTo to all groups which userFrom is a member.'''
+# Useful when migrating to Enterprise Logins
+# Reassigns all items/groups to new owner
+# Adds userTo to all groups which userFrom is a member
 
 from agoTools.admin import Admin
 myAgol = Admin('<username>')  # Replace <username> your ADMIN account
 
-# for migrating a single account...
-myAgol.migrateAccount(myAgol, '<userFrom>', '<userTo>')   # Replace with usernames between which you are moving items
+# un-comment one of the lines below, depending on which workflow you wish to use
 
-# for migrating a batch of accounts...
-myAgol.migrateAccounts(myAgol, <path to user mapping CSV>)   # Replace with path to CSV file with col1=userFrom, col2=userTo
+### for migrating a single account...
+# myAgol.migrateAccount(myAgol, '<userFrom>', '<userTo>')   # Replace with usernames between which you are moving items
+
+### for migrating multiple accounts...
+# myAgol.migrateAccounts(myAgol, <path to user mapping CSV>)   # Replace with path to CSV file with col1=userFrom, col2=userTo
